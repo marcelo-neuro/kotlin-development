4f192d059ef8b21255091daf61850c5975dd8189
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-09 11:51:07 -0300

    Feat last match

--- Placar/app/src/main/java/com/example/placar/PlayerActivity.kt
+++ Placar/app/src/main/java/com/example/placar/PlayerActivity.kt
@@ -4,13 +4,30 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.placar.databinding.ActivityPlayerBinding
+import utils.KEY_RESULT_EXTRA_PLAYER_ONE_NAME
+import utils.KEY_RESULT_EXTRA_PLAYER_ONE_SCORE
+import utils.KEY_RESULT_EXTRA_PLAYER_TWO_NAME
+import utils.KEY_RESULT_EXTRA_PLAYER_TWO_SCORE
 
 class PlayerActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPlayerBinding
+
+    private val previewRequest =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if(it.resultCode == RESULT_OK) {
+                val lastResult = getString(R.string.message_to_share,
+                    it.data?.getStringExtra(KEY_RESULT_EXTRA_PLAYER_ONE_NAME),
+                    it.data?.getStringExtra(KEY_RESULT_EXTRA_PLAYER_TWO_NAME),
+                    it.data?.getIntExtra(KEY_RESULT_EXTRA_PLAYER_ONE_SCORE, 0),
+                    it.data?.getIntExtra(KEY_RESULT_EXTRA_PLAYER_TWO_SCORE, 0))
+                binding.tvLastGame.text = lastResult
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
